832049e03108dc735577d20a18fb40062d583145
Mintlord3ttah <mintlordettah@gmaill.coil.com>
2024-12-31 02:25:09 -0800

    ammend

--- chef-diary/src/UI/Nav.jsx
+++ chef-diary/src/UI/Nav.jsx
@@ -1,17 +1,17 @@
 import AddRecipe from "../FEATURES/ADD_RECIPE/AddRecipe";
 import Logo from "./Logo";
-import NavList from "./NavListItem";
+import NavListItem from "./NavListItem";
 
 export default function Nav() {
     const cursor = "hover:bg-green-950 cursor"
     return (
         <nav className="cstm-nav text-2xl text-green-100 bg-green-900 flex flex-row p-10 justify-between items-center">
             <Logo />
-            <ul className="flex flex-row gap-6 font-semibold">
-                <NavList dropDown={true}>BOOKMARK</NavList>
-                <NavList dropDown={true} component={<AddRecipe />}>ADD RECIPE</NavList>
-                <NavList>ACCOUNT</NavList>
-                <NavList dropDown={true}>LEARN MORE</NavList>
+            <ul className="cstm-h-nav flex flex-row gap-6 font-semibold">
+                <NavListItem dropDown={true}>BOOKMARK</NavListItem>
+                <NavListItem dropDown={true} component={<AddRecipe />}>ADD RECIPE</NavListItem>
+                <NavListItem>ACCOUNT</NavListItem>
+                <NavListItem dropDown={true}>LEARN MORE</NavListItem>
             </ul>
         </nav>
     )
