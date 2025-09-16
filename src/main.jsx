20c2cd05f6856a4a8f6548ff684127ba649ccf6b
Mintlord3ttah <lordmint7@gmail.com>
2025-09-17 00:06:20 +0100

    remove clerk.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './clerk.css'
 import App from './App.jsx'
 import {ClerkProvider} from '@clerk/clerk-react'
 
